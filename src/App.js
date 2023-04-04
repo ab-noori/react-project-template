cf63f65a7ad64c31005d8b0ec732dc4a348ff668
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-04-04 23:37:48 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
+          My first react project
         </p>
         <a
           className="App-link"
